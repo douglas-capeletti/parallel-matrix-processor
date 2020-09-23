7f6f47ad83fca14fcb0b6323ea4785abcb52995d
Douglas Paz <douglas.drpaz@southsystem.com.br>
2020-09-23 01:01:56 -0300

    manage thread count and matrix length

--- src/matrix/Main.java
+++ src/matrix/Main.java
@@ -2,7 +2,30 @@ package matrix;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Matrix.run(Integer.parseInt(args[0]), true);
+    public static void main(String[] args) throws Exception {
+
+        int matrixLength = 0;
+        String threadFormat = "100%";
+        switch (args.length) {
+            case 0:
+                System.err.println("Missing parameters [matrix_length] <thread_number/thread_percentage>");
+                System.exit(1);
+            case 2:
+                threadFormat = args[1];
+            case 1:
+                matrixLength = Integer.parseInt(args[0]);
+        }
+
+        Matrix.run(matrixLength, Math.min(parseFormat(threadFormat), matrixLength));
+    }
+
+    private static int parseFormat(String threadFormat) {
+        int totalThreads = Runtime.getRuntime().availableProcessors();
+        if (threadFormat.endsWith("%")) {
+            int percent = Integer.parseInt(threadFormat.substring(0, threadFormat.length() - 1));
+            return totalThreads / (100 / percent);
+        }
+        int threadRequested = Integer.parseInt(threadFormat);
+        return Math.min(threadRequested, totalThreads);
     }
 }
--- src/matrix/Matrix.java
+++ src/matrix/Matrix.java
@@ -1,23 +1,21 @@
 package matrix;
 
+import java.util.concurrent.RunnableFuture;
+
 public class Matrix {
 
     // ESTRUTURAS DE DADOS COMPARTILHADA
-    public static int[][] m1;
-    public static int[][] m2;
-    public static int[][] mres;
+    public static volatile int[][] m1;
+    public static volatile int[][] m2;
+    public static volatile int[][] mres;
 
-    public static void run(int size){
-        run(size, false);
-    }
-
-    public static void run(int size, boolean print) {
+    public static void run(int size, int threadNumber) {
 
         // INICIALIZA OS ARRAYS A SEREM MULTIPLICADOS
         m1 = new int[size][size];
         m2 = new int[size][size];
         mres = new int[size][size];
-        if (m1[0].length != m2.length || mres.length != m1.length || mres[0].length != m2[0].length) {
+        if (m1[0].length != m2.length || mres[0].length != m2[0].length) {
             System.err.println("Impossivel multiplicar matrizes: parametros invalidos.");
             System.exit(1);
         }
@@ -45,15 +43,8 @@ public class Matrix {
         // PREPARA PARA MEDIR TEMPO
         long inicio = System.nanoTime();
 
-        // REALIZA A MULTIPLICACAO
-        for (int i = 0; i < mres.length; i++) {
-            for (int j = 0; j < mres[0].length; j++) {
-                mres[i][j] = 0;
-                for (k = 0; k < m2.length; k++) {
-                    mres[i][j] += m1[i][k] * m2[k][j];
-                }
-            }
-        }
+        //Efetua a multiplicação
+        multiply();
 
         // OBTEM O TEMPO
         long fim = System.nanoTime();
@@ -86,27 +77,19 @@ public class Matrix {
 
         // MOSTRA O TEMPO DE EXECUCAO
         System.out.println(total);
-
-        if(print){
-            System.out.println("\n------ Matriz 1 ------");
-            System.out.println(print(m1));
-            System.out.println("\n------ Matriz 2 ------");
-            System.out.println(print(m2));
-            System.out.println("\n------ Matriz R ------");
-            System.out.println(print(mres));
-        }
-
     }
 
-    public static String print(int[][] matriz){
-        StringBuilder saida= new StringBuilder();
-        for (int[] ints : matriz) {
-            for (int coluna = 0; coluna < matriz[0].length; coluna++) {
-                saida.append(ints[coluna]).append(" ");
+    private static void multiply() {
+        int k;
+        // REALIZA A MULTIPLICACAO
+        for (int i = 0; i < mres.length; i++) {
+            for (int j = 0; j < mres[0].length; j++) {
+                mres[i][j] = 0;
+                for (k = 0; k < m2.length; k++) {
+                    mres[i][j] += m1[i][k] * m2[k][j];
+                }
             }
-            saida.append("\n");
         }
-        return saida.toString();
     }
 
 }
